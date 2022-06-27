c8a6c829a241bc04545622b9f9e7d8c89c5c0650
lotys <danieladrm17@yahoo.com>
2022-06-27 15:58:25 +0300

    Start first part

--- src/test/java/base/addCartTest.java
+++ src/test/java/base/addCartTest.java
@@ -4,9 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 public class addCartTest extends ShareDataSaucedemo{
 
+
     @Test
     public void addToCart(){
 
+        WebElement sauceLabsBackPackadd= driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
+        sauceLabsBackPackadd.click();
 
     }
 }
--- src/test/java/base/loginPageTest.java
+++ src/test/java/base/loginPageTest.java
@@ -4,10 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 
-public class loginTest extends ShareDataSaucedemo {
+public class loginPageTest extends ShareDataSaucedemo {
 
     @Test
-    public void loginPageTest() {
+    public void loginTest() {
 
         WebElement standarduserElement = driver.findElement(By.cssSelector("[placeholder='Username']"));
         String standarduservalue = "standard_user";
@@ -20,8 +20,7 @@ public class loginTest extends ShareDataSaucedemo {
         WebElement submitElement= driver.findElement(By.cssSelector("[type='submit']"));
         submitElement.click();
 
-        WebElement sauceLabsBackPackadd= driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
-        sauceLabsBackPackadd.click();
+      
 
     }
 }
