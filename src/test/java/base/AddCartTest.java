066ec6af625dadfd294141bc908aeb46b9c1151c
lotys <danieladrm17@yahoo.com>
2022-06-28 14:36:31 +0300

    Start first part

--- src/test/java/base/AddCartTest.java
+++ src/test/java/base/AddCartTest.java
@@ -2,11 +2,13 @@ package base;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-public class addCartTest extends ShareDataSaucedemo{
+
+public class AddCartTest extends ShareDataSaucedemo{
 
 
     @Test
-    public void addToCart(){
+
+    public void ddToCart(){
 
         WebElement sauceLabsBackPackadd= driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
         sauceLabsBackPackadd.click();
